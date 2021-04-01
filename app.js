5666f88faa54cf54a5735e19a528a95b991e8641
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-04-02 02:49:08 +0300

    added the array of objects

--- app.js
+++ app.js
@@ -1,3 +1,84 @@
-function foo() {
-  console.log('oh');
-}
+// array that holds the cards
+const cardArray = [
+  {
+    name: 'blank',
+    img: './images/blank.png',
+  },
+
+  {
+    name: 'blank',
+    img: './images/blank.png',
+  },
+
+  {
+    name: 'cheeseburger',
+    img: './images/cheeseburger.png',
+  },
+
+  {
+    name: 'cheeseburger',
+    img: './images/cheeseburger.png',
+  },
+
+  {
+    name: 'fries',
+    img: './images/fries.png',
+  },
+
+  {
+    name: 'fries',
+    img: './images/fries.png',
+  },
+
+  {
+    name: 'hotdog',
+    img: './images/hotdog.png',
+  },
+
+  {
+    name: 'hotdog',
+    img: './images/hotdog.png',
+  },
+
+  {
+    name: 'ice-cream',
+    img: './images/ice-cream.png',
+  },
+
+  {
+    name: 'ice-cream',
+    img: './images/ice-cream.png',
+  },
+
+  {
+    name: 'milkshake',
+    img: './images/milkshake.png',
+  },
+
+  {
+    name: 'milkshake',
+    img: './images/milkshake.png',
+  },
+
+  {
+    name: 'pizza',
+    img: './images/pizza.png',
+  },
+
+  {
+    name: 'pizza',
+    img: './images/pizza.png',
+  },
+
+  {
+    name: 'white',
+    img: './images/white.png',
+  },
+
+  {
+    name: 'white',
+    img: './images/white.png',
+  },
+
+];
+console.log(cardArray.length);
